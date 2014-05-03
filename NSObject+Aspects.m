b7884c0d2757ab1013c394ef68ace758d0e0ac02
Peter Steinberger <steipete@gmail.com>
2014-05-03 21:04:48 +0200

    update header

--- NSObject+Aspects.m
+++ NSObject+Aspects.m
@@ -1,13 +1,8 @@
 //
 //  NSObject+Aspects.m
-//  PSPDFKit
+//  Aspects
 //
-//  Copyright (c) 2013-2014 PSPDFKit GmbH. All rights reserved.
-//
-//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY INTERNATIONAL COPYRIGHT LAW
-//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
-//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
-//  This notice may not be removed from this file.
+//  Copyright (c) 2014 Peter Steinberger. Licensed under the MIT license.
 //
 
 #import "NSObject+Aspects.h"
