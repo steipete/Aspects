b4bc1e6eb41b5386ecf8184891527ec0c9834f81
Nikolay Tymchenko <t.nick.a@gmail.com>
2014-05-10 03:24:20 +0200

    Initial implementation for aspect blocks with typed arguments

--- AspectsDemo/AspectsDemo/AspectsViewController.m
+++ AspectsDemo/AspectsDemo/AspectsViewController.m
@@ -18,16 +18,16 @@
     [self presentViewController:testController animated:YES completion:NULL];
 
     // We are interested in being notified when the controller is being dismissed.
-    [testController aspect_hookSelector:@selector(viewWillDisappear:) withOptions:0 usingBlock:^(id instance, NSArray *arguments) {
-        UIViewController *controller = instance;
+    [testController aspect_hookSelector:@selector(viewWillDisappear:) withOptions:0 usingBlock:^(id<AspectInfo> info, BOOL animated) {
+        UIViewController *controller = [info instance];
         if (controller.isBeingDismissed || controller.isMovingFromParentViewController) {
             [[[UIAlertView alloc] initWithTitle:@"Popped" message:@"Hello from Aspects" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Ok", nil] show];
         }
     } error:NULL];
 
     // Hooking dealloc is delicate, only AspectPositionBefore will work here.
-    [testController aspect_hookSelector:NSSelectorFromString(@"dealloc") withOptions:AspectPositionBefore usingBlock:^(id instance, NSArray *arguments) {
-        NSLog(@"Controller is about to be deallocated: %@", instance);
+    [testController aspect_hookSelector:NSSelectorFromString(@"dealloc") withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> info) {
+        NSLog(@"Controller is about to be deallocated: %@", [info instance]);
     } error:NULL];
 }
 
