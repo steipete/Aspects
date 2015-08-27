93cd421cf96f7d9bc41fdafdca7cbe28267e04c4
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-08-26 18:59:50 -0700

    added watchOS target.

---
+++ Aspects-watchOS/Aspects-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Aspects-watchOS.h
+//  Aspects-watchOS
+//
+//  Created by Jeffrey Bergier on 8/26/15.
+//  Copyright © 2015 Peter Steinberger. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Aspects-watchOS.
+FOUNDATION_EXPORT double Aspects_watchOSVersionNumber;
+
+//! Project version string for Aspects-watchOS.
+FOUNDATION_EXPORT const unsigned char Aspects_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aspects_watchOS/PublicHeader.h>
+
+
